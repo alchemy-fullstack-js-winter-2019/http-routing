34bff444c28dfbc6b6e6e46623ff7d2707271ae8
Paige Gorry <paigeegorry@gmail.com>
2019-01-17 10:48:21 -0800

    get method for people passing

--- test/app.test.js
+++ test/app.test.js
@@ -3,6 +3,16 @@ const request = require('supertest');
 const rimraf = require('rimraf');
 const mkdirp = require('mkdirp');
 
+const createPerson = (name) => {
+  return request(app)
+    .post('/people')
+    .send({
+      name: name,
+      age: '100',
+      favoriteColor: 'red'
+    });
+};
+
 describe('people', () => {
   beforeEach(done => {
     rimraf('./data/people', err => {
@@ -24,18 +34,22 @@ describe('people', () => {
       })
       .then(res => {
         expect(res.body).toEqual({
-          name: 'Uncle Jim',
+          name: 'Uncle Bob',
           age: '100',
           favoriteColor: 'red',
           _id: expect.any(String)
         });
       });
   });
-  it('can get all people', () => {
-    return request(app)
-      .get('/people')
+  it('can get a list of people from our db', () => {
+    const namesToCreate = ['paige', 'bob', 'jim', 'frank', 'billy'];
+    return Promise.all(namesToCreate.map(createPerson))
+      .then(() => {
+        return request(app)
+          .get('/people');
+      })
       .then(res => {
-        expect(res.body).toContain('Uncle Bob');
+        expect(res.body).toHaveLength(5);
       });
   });
 });
