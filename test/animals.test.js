36f737697aaae4a9d62bc50766ed2f63f9fdb8cd
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-17 15:22:00 -0800

    separated tests and added in tasks tests, all 20 passing

---
+++ test/animals.test.js
@@ -0,0 +1,106 @@
+const request = require('supertest');
+const app = require('../lib/app');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+
+const createAnimal = (name) => {
+  return request(app)
+    .post('/animals')
+    .send({ 
+      name: name,
+      type: 'mammal'
+    })
+    .then(res => res.body);
+};
+
+describe('animal tests', () => {
+
+  beforeEach((done) => {
+    rimraf('./data/animals', err => {
+      done(err);
+    });
+  });
+
+  beforeEach((done) => {
+    mkdirp('./data/animals', err => {
+      done(err);
+    });
+  });
+
+  it('creates an animal', () => {
+    return request(app)
+      .post('/animals')
+      .send({ 
+        name: 'tiger',
+        type: 'mammal'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'tiger',
+          type: 'mammal',
+          _id: expect.any(String)
+        });
+      });
+  });
+
+  it('can list all the animals in the database', () => {
+    const names = ['dolphin', 'panda', 'tiger', 'wolf'];
+    return Promise.all(names.map(createAnimal))
+      .then(() => {
+        return request(app)
+          .get('/animals');
+      })
+      .then(({ body }) => {
+        console.log(body);
+        expect(body).toHaveLength(4);
+      });
+  });
+
+  it('gets an animal by id', () => {
+    return createAnimal('tiger')
+      .then((createdAnimal) => {
+        const id = createdAnimal._id;
+        return request(app)
+          .get(`/animals/${id}`);
+      })
+      .then(res => {
+        expect(res.body.name).toContain('tiger');
+      });
+  });
+
+  it('updates an animal with :id and returns the update', () => {
+    return createAnimal('tiger')
+      .then((createdAnimal) => {
+        console.log(createdAnimal);
+        const id = createdAnimal._id;
+        const updatedObject = {
+          name: 'bengal tiger',
+          type: 'mammal'
+        };
+        return request(app)
+          .put(`/animals/${id}`)
+          .send(updatedObject)
+          .then(() => {
+            return request(app)
+              .get(`/animals/${id}`)
+              .then(res => {
+                console.log(res.body);
+                expect(res.body.name).toContain('bengal tiger');
+              });
+          });
+      });
+  });
+
+  it('deletes an animal with :id and returns the delete count', () => {
+    return createAnimal('koala')
+      .then((createdAnimal) => {
+        console.log(createdAnimal);
+        const id = createdAnimal._id;
+        return request(app)
+          .delete(`/animals/${id}`)
+          .then(res => {
+            expect(res.body).toEqual({ 'deleted': 1 });
+          });
+      });
+  });
+});
---
+++ test/people.test.js
@@ -0,0 +1,109 @@
+const request = require('supertest');
+const app = require('../lib/app');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+
+const createPerson = (name) => {
+  return request(app)
+    .post('/people')
+    .send({ 
+      name: name,
+      age: 99,
+      favoriteColor: 'black'
+    })
+    .then(res => res.body);
+};
+
+describe('people tests', () => {
+  beforeEach((done) => {
+    rimraf('./data/people', err => {
+      done(err);
+    });
+  });
+
+  beforeEach((done) => {
+    mkdirp('./data/people', err => {
+      done(err);
+    });
+  });
+
+  it('creates a person', () => {
+    return request(app)
+      .post('/people')
+      .send({ 
+        name: 'Eminem',
+        age: 35,
+        favoriteColor: 'black'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'Eminem',
+          age: 35,
+          favoriteColor: 'black',
+          _id: expect.any(String)
+        });
+      });
+  });
+
+  it('can list all the people in the database', () => {
+    const names = ['kristin1', 'kristin2', 'kristin3', 'kristin4'];
+    return Promise.all(names.map(createPerson))
+      .then(() => {
+        return request(app)
+          .get('/people');
+      })
+      .then(({ body }) => {
+        console.log(body);
+        expect(body).toHaveLength(4);
+      });
+  });
+
+  it('gets a person by id', () => {
+    return createPerson('kristin1')
+      .then((createdPerson) => {
+        const id = createdPerson._id;
+        return request(app)
+          .get(`/people/${id}`);
+      })
+      .then(res => {
+        expect(res.body.name).toContain('kristin1');
+      });
+  });
+
+  it('updates a person with :id and returns the update', () => {
+    return createPerson('kristin1')
+      .then((createdPerson) => {
+        console.log(createdPerson);
+        const id = createdPerson._id;
+        const updatedObject = {
+          name: 'roxius',
+          age: 99,
+          favoriteColor: 'black'
+        };
+        return request(app)
+          .put(`/people/${id}`)
+          .send(updatedObject)
+          .then(() => {
+            return request(app)
+              .get(`/people/${id}`)
+              .then(res => {
+                console.log(res.body);
+                expect(res.body.name).toContain('roxius');
+              });
+          });
+      });
+  });
+  
+  it('deletes a person with :id and returns the delete count', () => {
+    return createPerson('kristin1')
+      .then((createdPerson) => {
+        const id = createdPerson._id;
+        return request(app)
+          .delete(`/people/${id}`)
+          .then(res => {
+            expect(res.body).toEqual({ 'deleted': 1 });
+          });
+      });
+  });
+
+});
