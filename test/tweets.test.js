4d4801248a994e01e7d8931825515a43b1376841
Kate Dameron <katerj33@gmail.com>
2019-01-17 15:13:02 -0800

    updated tweets test with two before each blocks

--- test/tweets.test.js
+++ test/tweets.test.js
@@ -4,6 +4,28 @@ const mkdirp = require('mkdirp');
 const request = require('supertest');
 const rimraf = require('rimraf');
 
-describe('tweets test', () => {
+const makeTweet = (text) => {
+  return request(app)
+    .post('/people')
+    .send({
+      name: 'katerj',
+      tweet: 'Yay, tomorrow is Friday'
+    })
+    .then(res => res.body);
+};
 
+describe('tweets test', () => {
+  beforeEach(done => {
+    // Before each test remove directory so we don't have old test data
+    rimraf('./data/tweets', err => {
+      done(err);
+    });
+  });
+
+  beforeEach(done => {
+    // Before each make a new directory to store new test data
+    mkdirp('./data/tweets', err => {
+      done(err);
+    });
+  });
 });
