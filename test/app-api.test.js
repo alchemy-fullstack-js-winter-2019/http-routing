94680d39a8a06d3cc362f9c23bdea941893d1121
Shaba9 <shaba92@gmail.com>
2019-01-17 22:07:53 -0800

    eslint stuff

--- test/app-api.test.js
+++ test/app-api.test.js
@@ -1,17 +1,19 @@
 const request = require('superagent');
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
+// const shortId = require('shortid');
 
-const createPerson = name => {
-  return request
-    .post('http://localhost:7890/people')
-    .send({
-      name,
-      age: 99,
-      color: 'red'
-    })
-    .then(res => console.log('res', res.request._data));
-};
+// const createPerson = name => {
+//   return request
+//     .post('http://localhost:7890/people')
+//     .send({
+//       name,
+//       age: 99,
+//       color: 'red',
+//       id: shortId.generate()
+//     })
+//     .then(res => res.request._data);
+// };
 
 
 describe('app tests', () => {
@@ -28,11 +30,12 @@ describe('app tests', () => {
   });
 
   it('gets list of people', () => {
-    return createPerson('shabz')
-      .then(person => {
-        request
-          .get('http://localhost:7890/people')
-          .then(result => expect(JSON.parse(result.text)).toEqual(person));
-      });
+    // return createPerson('shabz')
+    //   .then(person => {
+    request
+      .get('http://localhost:7890/people')
+      .then(result => expect(JSON.parse(result.text)).toEqual([{ 'id':0, 'name':'Bob', 'age':104, 'color':'brown' }, { 'id':1, 'name':'Shirly', 'age':18, 'color':'blue' }]
+      ));
+    // });
   });
 });
