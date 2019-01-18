585ea3e6794245c6ccb3defa505e3fb37c455d11
Paige Gorry <paigeegorry@gmail.com>
2019-01-17 17:21:58 -0800

    request not found test

--- test/app.test.js
+++ test/app.test.js
@@ -1,1 +1,12 @@
-// what goes in app.test??
+const app = require('../lib/app');
+const request = require('supertest');
+
+describe('routes', () => {
+  it('displays an error when path not found', () => {
+    return request(app)
+      .get('/banana')
+      .then(res => {
+        expect(res.statusCode).toEqual(404);
+      });
+  });
+});
