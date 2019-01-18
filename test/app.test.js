310112f3cd0e4bbb45a14b6be7cb68eccc7c6c18
JeiWolfei <jvampy22@gmail.com>
2019-01-17 17:37:17 -0800

    copied to people.js and starting to add other models

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,18 @@
-onst mkdirp = require('mkdirp');
+const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
 const request = require('supertest');
 const app = require('../lib/app');
+
+const createPerson = name => {
+  return request(app)
+    .post('./people')
+    .send({
+      name,
+      age: 100,
+      favoriteColor: 'red'
+    })
+    .then(res => res.body);
+}; 
 
 describe('app tests', () => {
   beforeEach(done => {
@@ -32,5 +43,68 @@ describe('app tests', () => {
           _id: expect.any(String)
         });
       });
+  });
+
+  it('gets a list of people from our db', ()=> {
+    const namesToCreate = ['jei', 'jj', 'jbird', 'jroc'];
+    return Promise.all(namesToCreate.map(createPerson))
+      .then(() => {
+        return request(app)
+          .get('/people');
+      })
+      .then(({ body }) => {
+        expect(body).toHaveLength(4);
+      });
+  });
+
+  it('gets a person by id', () => {
+    return createPerson('jei')
+      .then(({ _id }) => {
+        return Promise.all([
+          Promise.resolve(_id),
+          request(app).get(`/people/${_id}`)
+        ]);
+      })
+      .then(([_id, { body }]) => {
+        expect(body).toEqual({
+          name: 'jei',
+          age: 40,
+          favoriteColor: 'blue',
+          _id
+        });
+      });
+
+    it('can find by id and update', () => {
+      return createPerson('jj')
+        .then(({ body }) => {
+          return request(app)
+            .put(`/people/${body._id}`);
+        })
+        .then(({ body }) => {
+          return Promise.all([
+            request(app)
+              .get(`/people/${body._id}`, () => {
+              })
+          ]);
+        })
+        .then(([_id, { body }]) => {
+          expect(body).toEqual({
+            name: 'jbird',
+            age: '99',
+            favoriteColor: 'blackheart',
+            _id
+          });
+        });
+    });
+    it('can delete a person', () => {
+      return createPerson('jello')
+        .then(({ body }) => {
+          return request(app)
+            .delete(`/people/${body._id}`);
+        })
+        .then(({ body }) => {
+          expect(body).toEqual({ deleted: 1 });
+        });
+    });
   });
 });
