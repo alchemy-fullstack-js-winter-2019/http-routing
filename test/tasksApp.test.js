8814dde64415d81805aa1dc1a9d86c4a4a25c74a
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-01-18 12:24:21 -0800

    finsished Tasks and added starwars api

--- test/tasksApp.test.js
+++ test/tasksApp.test.js
@@ -3,12 +3,12 @@ const app = require('../lib/app');
 const rimraf = require('rimraf');
 const mkdirp = require('mkdirp');
 
-const createTask = (task, completed) => {
+const createTask = (task, ) => {
   return request(app)
     .post('/tasks')
     .send({
       task: task,
-      completed: completed
+      completed: 'no'
     })
   .then(res => res.body);
 };
@@ -37,4 +37,66 @@ describe('tasks', () => {
         })
       })
   })
+  it('gets a list of all the tasks', () => {
+    const tasks = ['wash car', 'buy groceries', 'walk the cats'];
+    return Promise.all(tasks.map(createTask))
+      .then(() => {
+        return request(app)
+          .get('/tasks')
+      })
+      .then(({ body }) => {
+        expect(body).toHaveLength(3);
+      })
+  })
+  it('gets a task by id', () => {
+    return createTask('go to the gym')
+      .then(createdTask => {
+        const id = createdTask._id;
+        return request(app) 
+          .get(`/tasks/${id}`)
+      })
+      .then(({ body }) => {
+        expect(body).toEqual({
+          task: 'go to the gym',
+          completed: 'no',
+          _id: expect.any(String)
+        })
+      })
+  })
+  it('updates by id', () => {
+    return createTask('spend lots of money')
+      .then(createdTask => {
+        const id = createdTask._id;
+        return request(app)
+          .put(`/tasks/${id}`)
+          .send({
+            task: 'spend lots of dough',
+            completed: 'no',
+            _id: id
+          })
+          .then(() => {
+            return request(app)
+              .get(`/tasks/${id}`)
+              .then(res => {
+                expect(res.body).toEqual({
+                  task: 'spend lots of dough',
+                  completed: 'no',
+                  _id: id
+                })
+              })
+          })
+      })
+  })
+  it('deletes by id', () => {
+    return createTask('deletedTask')
+      .then(createdTask => {
+        const id = createdTask._id;
+        return request(app)
+          .delete(`/tasks/${id}`)
+      })
+      .then(res => {
+        expect(res.body).toEqual({ deleted: 1 })
+      })
+  })
+  
 })
