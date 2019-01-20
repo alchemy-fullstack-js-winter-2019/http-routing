c91d7dbfdf32605bf58b7683316efae34289d799
Shaba9 <shaba92@gmail.com>
2019-01-20 12:47:48 -0800

    beforeEach added; post and get pass

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,34 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+let person = null;
 describe('app', () => {
+  beforeEach(done => {
+    request(app)
+      .post('/people')
+      .send({
+        id: 1,
+        name: 'shabz',
+        age: 26,
+        color: 'red'
+      })
+      .then(res => {
+        person = JSON.parse(res.text);
+        console.log('person', person);
+        done();
+      });
+  });
+
   it('gets list of people', () => {
     return request(app)
       .get('/people')
-      .then(res => expect(JSON.parse(res.text)).toEqual([
-        { 'id':0, 'name':'Bob', 'age':104, 'color':'brown' },
-        { 'id':1, 'name':'Shirly', 'age':18, 'color':'blue' }
-      ]));
+      .then(res => expect(JSON.parse(res.text)).toEqual({
+        1: {
+          id: 1,
+          name: 'shabz',
+          age: 26,
+          color: 'red'
+        }
+      }));
   });
 });
--- test/app2test.js
+++ test/app2test.js
@@ -1,42 +1,42 @@
-const request = require('superagent');
-const mkdirp = require('mkdirp');
-const rimraf = require('rimraf');
-// const shortId = require('shortid');
+// const request = require('superagent');
+// const mkdirp = require('mkdirp');
+// const rimraf = require('rimraf');
+// // const shortId = require('shortid');
 
-// const createPerson = name => {
-//   return request
-//     .post('http://localhost:7890/people')
-//     .send({
-//       name,
-//       age: 99,
-//       color: 'red',
-//       id: shortId.generate()
-//     })
-//     .then(res => res.request._data);
-// };
+// // const createPerson = name => {
+// //   return request
+// //     .post('http://localhost:7890/people')
+// //     .send({
+// //       name,
+// //       age: 99,
+// //       color: 'red',
+// //       id: shortId.generate()
+// //     })
+// //     .then(res => res.request._data);
+// // };
 
 
-describe('app tests', () => {
-  beforeEach(done => {
-    rimraf('./data/people', err => {
-      done(err);
-    });
-  });
+// describe('app tests', () => {
+//   beforeEach(done => {
+//     rimraf('./data/people', err => {
+//       done(err);
+//     });
+//   });
 
-  beforeEach(done => {
-    mkdirp('./data/people', err => {
-      done(err);
-    });
-  });
+//   beforeEach(done => {
+//     mkdirp('./data/people', err => {
+//       done(err);
+//     });
+//   });
 
-  it('gets list of people', () => {
-    // return createPerson('shabz')
-    //   .then(person => {
-    request
-      .get('http://localhost:7890/people')
-      .then(result => expect(JSON.parse(result.text)).toEqual([{ 'id':0, 'name':'Bob', 'age':104, 'color':'brown' }, { 'id':1, 'name':'Shirly', 'age':18, 'color':'blue' }]
-      ));
-    // });
-  });
+//   it('gets list of people', () => {
+//     // return createPerson('shabz')
+//     //   .then(person => {
+//     request
+//       .get('http://localhost:7890/people')
+//       .then(result => expect(JSON.parse(result.text)).toEqual([{ 'id':0, 'name':'Bob', 'age':104, 'color':'brown' }, { 'id':1, 'name':'Shirly', 'age':18, 'color':'blue' }]
+//       ));
+//     // });
+//   });
 
-});
+// });
