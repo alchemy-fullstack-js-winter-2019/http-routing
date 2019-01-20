2384981cf89d9857f17d58dc202416c08fa8342e
Kate Dameron <katerj33@gmail.com>
2019-01-20 11:36:19 -0800

    add new tasks post passing test

--- test/models/tasks.test.js
+++ test/models/tasks.test.js
@@ -17,15 +17,32 @@ const makeTask = (title) => {
 describe('Tasks', () => {
 
   beforeEach(done => {
-    rimraf('./data/tasks');
-    done();
+    rimraf('./data/tasks', err => {
+      done(err);
+    });
   });
 
   beforeEach(done => {
-    mkdirp('./data/tasks');
-    done();
+    mkdirp('./data/tasks', err => {
+      done(err);
+    });
   });
 
-  
+  it('adds a new tasks', () => {
+    return request(app)
+      .post('/tasks')
+      .send({
+        title: 'task1',
+        description: 'add this task'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          title: 'task1',
+          description: 'add this task',
+          _id: expect.any(String)
+        });
+      });
+  });
 
 });
+
