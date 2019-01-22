e2116942d93376c91b713310add52ce437a83b4b
Shaba9 <shaba92@gmail.com>
2019-01-21 16:13:24 -0800

    get by id and put notFound tests pass

--- test/app.test.js
+++ test/app.test.js
@@ -32,6 +32,12 @@ describe('app', () => {
       .then(res => expect(JSON.parse(res.text)).toEqual(person));
   });
 
+  it('get person by id returns Not Found', () => {
+    return request(app)
+      .get('/people/3')
+      .then(res => expect(JSON.parse(res.text)).toEqual('Not Found'));
+  });
+
   it('updates a person by id', () => {
     return request(app)
       .put(`/people/${person.id}`)
@@ -47,6 +53,18 @@ describe('app', () => {
         age: 36,
         color: 'red'
       }));
+  });
+  
+  it('update returns Not Found', () => {
+    return request(app)
+      .put('/people/3')
+      .send({
+        id: 3,
+        name: 'shabster',
+        age: 36,
+        color: 'red'
+      })
+      .then(res => expect(JSON.parse(res.text)).toEqual('Not Found'));
   });
 
   it('deletes a person by id', () => {
